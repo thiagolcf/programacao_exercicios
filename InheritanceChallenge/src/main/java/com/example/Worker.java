864b6c4161779f18bd404b67d2695b9997137f26
thiagolcf <you@example.com>
2025-11-16 14:10:31 -0300

    Worker Class

---
+++ InheritanceChallenge/src/main/java/com/example/Worker.java
@@ -0,0 +1,29 @@
+package com.example;
+
+import java.time.Year;
+
+public class Worker {
+  private String name;
+  private String birthDate;
+  protected String endDate;
+
+  public Worker(String name, String birthDate) {
+    this.name = name;
+    this.birthDate = birthDate;
+  }
+
+  public int getAge() {
+    int currentYear = Year.now().getValue();
+    int birthYear = Integer.parseInt(birthDate.substring(6));
+    return currentYear - birthYear;
+  }
+
+  public double collectPay() {
+    return 0.0;
+  }
+
+  public void terminate(String endDate) {
+    this.endDate = endDate;
+  }
+
+}
